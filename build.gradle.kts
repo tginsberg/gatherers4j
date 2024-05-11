5defc0a84c75bb8f83a660094a4d576a5015af1b
Todd Ginsberg <todd@ginsberg.com>
2024-05-11 11:55:45 -0400

    Adding maven publication
    +TODO: Remove javadoc warning exclusion
    +TODO: Add publication repositories
    +TODO: Add jar signing
    +Add: Gradle maven-publish plugin and associated configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
+    id("com.adarshr.test-logger") version "4.0.0"
     id("jacoco")
     id("java-library")
-    id("com.adarshr.test-logger") version "4.0.0"
+    id("maven-publish")
 }
 
 description = "An extra set of helpful Stream Gatherers for Java"
@@ -15,6 +16,8 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(22)
     }
+    withJavadocJar()
+    withSourcesJar()
 }
 
 repositories {
@@ -33,16 +36,70 @@ dependencies {
     }
 }
 
-tasks.withType<JavaCompile> {
-    options.compilerArgs.add(ENABLE_PREVIEW)
+publishing {
+    publications {
+        create<MavenPublication>("gatherers4j") {
+            from(components["java"])
+            pom {
+                name = "Gatherers4J"
+                description = project.description
+                url = "https://github.com/tginsberg/gatherers4j"
+                organization {
+                    name = "com.ginsberg"
+                    url = "https://github.com/tginsberg"
+                }
+                issueManagement {
+                    system = "GitHub"
+                    url = "https://github.com/tginsberg/gatherers4j/issues"
+                }
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "tginsberg"
+                        name = "Todd Ginsberg"
+                        email = "todd@ginsberg.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:https://github.com/tginsberg/gatherers4j.git"
+                    developerConnection = "scm:git:https://github.com/tginsberg/gatherers4j.git"
+                    url = "https://github.com/tginsberg/gatherers4j"
+                }
+            }
+        }
+    }
 }
 
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-    jvmArgs(ENABLE_PREVIEW)
-    useJUnitPlatform()
+tasks {
+    withType<JavaCompile> {
+        options.compilerArgs.add(ENABLE_PREVIEW)
+    }
+    jacocoTestReport {
+        dependsOn(test)
+    }
+    jar {
+        manifest {
+            attributes(
+                "Implementation-Title" to "Gatherers4J",
+                "Implementation-Version" to archiveVersion
+            )
+        }
+    }
+    javadoc {
+        (options as CoreJavadocOptions).apply {
+            addStringOption("source", rootProject.java.toolchain.languageVersion.get().toString())
+            addBooleanOption("-enable-preview", true)
+            addStringOption("Xdoclint:none", "-quiet") // TODO: Remove this when we've documented things
+        }
+    }
+    test {
+        finalizedBy(jacocoTestReport)
+        jvmArgs(ENABLE_PREVIEW)
+        useJUnitPlatform()
+    }
 }
