b43a0804cd6eb1522b59152aaf2e9ba9abdca687
Todd Ginsberg <todd@ginsberg.com>
2024-11-01 17:09:59 -0400

    #48: Account for stream and argument not being the same length (#52)
    + I am not wild about having to use a type witness, but I'm fine with adding it as I think this use case is pretty niche and anybody who needs it could probably tolerate a type witness.

--- src/main/java/com/ginsberg/gatherers4j/ZipWithGatherer.java
+++ src/main/java/com/ginsberg/gatherers4j/ZipWithGatherer.java
@@ -18,6 +18,8 @@ package com.ginsberg.gatherers4j;
 
 import java.util.Iterator;
 import java.util.Spliterator;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.stream.Gatherer;
 import java.util.stream.Stream;
 
@@ -25,6 +27,8 @@ import static com.ginsberg.gatherers4j.GathererUtils.mustNotBeNull;
 
 public class ZipWithGatherer<FIRST, SECOND> implements Gatherer<FIRST, Void, Pair<FIRST, SECOND>> {
     private final Spliterator<SECOND> otherSpliterator;
+    private Function<SECOND, FIRST> sourceWhenArgumentLonger;
+    private Function<FIRST, SECOND> argumentWhenSourceLonger;
 
     ZipWithGatherer(final Iterable<SECOND> other) {
         mustNotBeNull(other, "Other iterable must not be null");
@@ -34,7 +38,7 @@ public class ZipWithGatherer<FIRST, SECOND> implements Gatherer<FIRST, Void, Pai
     ZipWithGatherer(final Iterator<SECOND> other) {
         mustNotBeNull(other, "Other iterator must not be null");
         final Iterable<SECOND> iterable = () -> other;
-        otherSpliterator = iterable.spliterator();
+        otherSpliterator = (iterable).spliterator();
     }
 
     ZipWithGatherer(final Stream<SECOND> other) {
@@ -42,11 +46,81 @@ public class ZipWithGatherer<FIRST, SECOND> implements Gatherer<FIRST, Void, Pai
         otherSpliterator = other.spliterator();
     }
 
+    /// When the argument `Iterable`, `Iterator` or `Stream` runs out of elements before the source stream does,
+    /// use the result of the `function` provided for the remaining `SECOND` elements of each `Pair`
+    /// until the source is exhausted.
+    ///
+    /// Note: You may need a type witness when using this:
+    ///
+    /// `source.gather(Gatherers4j.<String, Integer>zipWith(right).argumentWhenSourceLonger(String::length))`
+    ///
+    /// @param mappingFunction A non-null function which takes a possibly null `<FIRST>`
+    ///                        and emits a possibly null `<SECOND>`
+    public ZipWithGatherer<FIRST, SECOND> argumentWhenSourceLonger(final Function<FIRST, SECOND> mappingFunction) {
+        mustNotBeNull(mappingFunction, "Mapping function must not be null, use nullArgumentWhenSourceLonger() to insert nulls");
+        argumentWhenSourceLonger = mappingFunction;
+        return this;
+    }
+
+    /// When the source stream runs out of elements before the argument `Iterable`, `Iterator` or `Stream` does,
+    /// use the result of the `function` provided for the remaining `FIRST` elements of each `Pair`
+    /// until the argument is exhausted.
+    ///
+    /// Note: You may need a type witness when using this:
+    ///
+    /// `source.gather(Gatherers4j.<String, Integer>zipWith(right).sourceWhenArgumentLonger(String::valueOf))`
+    ///
+    /// @param mappingFunction A non-null function which takes a possibly null `<SECOND>`
+    ///                        and emits a possibly null `<FIRST>`
+    public ZipWithGatherer<FIRST, SECOND> sourceWhenArgumentLonger(final Function<SECOND, FIRST> mappingFunction) {
+        mustNotBeNull(mappingFunction, "Mapping function must not be null, use nullSourceWhenArgumentLonger() to insert nulls");
+        sourceWhenArgumentLonger = mappingFunction;
+        return this;
+    }
+
+    /// When the argument `Iterable`, `Iterator` or `Stream` runs out of elements before the source stream does,
+    /// use `null` for the remaining `SECOND` elements of each `Pair` until the source is exhausted.
+    ///
+    /// Note: You may need a type witness when using this:
+    ///
+    /// `source.gather(Gatherers4j.<String, Integer>zipWith(right).nullArgumentWhenSourceLonger())`
+    ///
+    public ZipWithGatherer<FIRST, SECOND> nullArgumentWhenSourceLonger() {
+        argumentWhenSourceLonger = _ -> null;
+        return this;
+    }
+    
+    /// When the source stream runs out of elements before the argument `Iterable`, `Iterator` or `Stream` does,
+    /// use `null` for the remaining `FIRST` elements of each `Pair` until the argument is exhausted.
+    ///
+    /// Note: You may need a type witness when using this:
+    ///
+    /// `source.gather(Gatherers4j.<String, Integer>zipWith(right).nullSourceWhenArgumentLonger())`
+    public ZipWithGatherer<FIRST, SECOND> nullSourceWhenArgumentLonger() {
+        sourceWhenArgumentLonger = _ -> null;
+        return this;
+    }
+
     @Override
     public Integrator<Void, FIRST, Pair<FIRST, SECOND>> integrator() {
-        return (_, element, downstream) -> otherSpliterator
-                .tryAdvance(
-                        it -> downstream.push(new Pair<>(element, it))
-                ) && !downstream.isRejecting();
+        return (_, element, downstream) -> {
+            boolean advanced = otherSpliterator.tryAdvance(it -> downstream.push(new Pair<>(element, it)));
+            if (!advanced && argumentWhenSourceLonger != null) {
+                downstream.push(new Pair<>(element, argumentWhenSourceLonger.apply(element)));
+            }
+            return !downstream.isRejecting();
+        };
+    }
+
+    @Override
+    public BiConsumer<Void, Downstream<? super Pair<FIRST, SECOND>>> finisher() {
+        return (_, downstream) -> {
+            boolean downstreamIsRejecting = downstream.isRejecting();
+            while (sourceWhenArgumentLonger != null && !downstreamIsRejecting) {
+                downstreamIsRejecting = !otherSpliterator.tryAdvance(arg ->
+                        downstream.push(new Pair<>(sourceWhenArgumentLonger.apply(arg), arg))
+                );
+            }
+        };
     }
 }
