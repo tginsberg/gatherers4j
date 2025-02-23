ac15b0b47f38ef58ecac66922b6aae711611c8af
Todd <todd@ginsberg.com>
2025-02-23 18:14:33 -0500

    Implement mapIndexed

--- src/test/java/com/ginsberg/gatherers4j/SimpleIndexingGatherersTest.java
+++ src/test/java/com/ginsberg/gatherers4j/SimpleIndexingGatherersTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.stream.Gatherers;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
